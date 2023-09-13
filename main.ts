0c53087f62b9a03dffef8cbc09549cfa561d81bb
rock204 <75961408+rock204@users.noreply.github.com>
2023-09-13 12:48:54 +0900

    修正-1　端子をタップしたときに変更

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
-function taiki () {
-    basic.pause(randint(1000, 5000))
-    mode = 3
-    basic.showIcon(IconNames.Heart)
-    st_time = input.runningTime()
-}
-function get_rt () {
-    if (input.pinIsPressed(TouchPin.P0)) {
+input.onPinPressed(TouchPin.P0, function () {
+    if (mode == 3) {
         mode = 4
         rt = (input.runningTime() - st_time) / 1000
         basic.clearScreen()
         basic.showNumber(rt)
         mode = 999
     }
+})
+function taiki () {
+    basic.pause(randint(1000, 5000))
+    mode = 3
+    st_time = input.runningTime()
+    basic.showIcon(IconNames.Heart)
 }
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Square)
@@ -22,8 +22,8 @@ function junbi () {
     basic.clearScreen()
     mode = 2
 }
-let rt = 0
 let st_time = 0
+let rt = 0
 let mode = 0
 basic.showString("RT Pro")
 mode = 999
@@ -34,9 +34,6 @@ basic.forever(function () {
         }
         if (mode == 2) {
             taiki()
-        }
-        if (mode == 3) {
-            get_rt()
         }
     }
 })
