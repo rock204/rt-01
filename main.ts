485eded5eb97861356b9d921612272d0b8b5724f
rock204 <75961408+rock204@users.noreply.github.com>
2023-09-26 08:05:44 +0900

    Aボタンを押してスタートして反応時間を取得、反応の取得はP1に接続したSW

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
-input.onPinPressed(TouchPin.P0, function () {
-    if (mode == 3) {
-        mode = 4
-        rt = (input.runningTime() - st_time) / 1000
-        basic.clearScreen()
-        basic.showNumber(rt)
-        mode = 999
-    }
-})
 function taiki () {
     basic.pause(randint(1000, 5000))
     mode = 3
     st_time = input.runningTime()
-    basic.showIcon(IconNames.Heart)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+}
+function get_rt () {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        if (mode == 3) {
+            mode = 4
+            rt = (input.runningTime() - st_time) / 1000
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            basic.showNumber(rt)
+            mode = 999
+        }
+    }
 }
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Square)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     mode = 1
 })
 function junbi () {
@@ -22,12 +24,16 @@ function junbi () {
     basic.clearScreen()
     mode = 2
 }
-let st_time = 0
 let rt = 0
+let st_time = 0
 let mode = 0
-basic.showString("RT Pro")
+basic.showString("RT ProGPIO")
 mode = 999
+pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
+pins.digitalWritePin(DigitalPin.P0, 0)
 basic.forever(function () {
+    // 測定前でない
+    // 
     if (mode != 999) {
         if (mode == 1) {
             junbi()
@@ -35,5 +41,6 @@ basic.forever(function () {
         if (mode == 2) {
             taiki()
         }
+        get_rt()
     }
 })
